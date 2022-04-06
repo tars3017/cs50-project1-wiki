a8dc9c43782c2bab8041ef334b76b7c7ad342609
Chuang Bing Cheng <tars3017@debian-BULLSEYE-live-builder-AMD64>
2022-04-06 22:58:49 +0800

    Try to full fill "Visiting /wiki/TITLE, where TITLE is the title of an encyclopedia entry, should render a page that displays the contents of that encyclopedia entry." but failed.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("css/", views.css, name="css"),
+    path("django/", views.django, name="django"),
+    path("git/", views.git, name="git"),
+    path("html/", views.html, name="html"),
+    path("python/", views.python, name="python")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,32 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown2 import Markdown
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def css(request):
+    with open("entries/CSS.md") as f:
+        s = f.read()   
+        print(markdowner.convert(s))
+        return render(request, "encyclopedia/show_result.html", {
+            "page_title": "CSS",
+            "content": markdowner.convert(s)
+        })
+
+def django(request):
+    return render(request, "encyclopedia/show_result.html")
+
+def git(request):
+    return render(request, "encyclopedia/show_result.html")
+
+def html(request):
+    return render(request, "encyclopedia/show_result.html")
+
+def python(request):
+    return render(request, "encyclopedia/show_result.html")
 
