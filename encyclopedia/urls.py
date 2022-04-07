6feae0373df900985b767dfcf48f2e2cde5ac156
Chuang Bing Cheng <tars3017@debian-BULLSEYE-live-builder-AMD64>
2022-04-07 20:41:34 +0800

    Make the link to other page work and add link to other page on index.html. Oh ya!

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("css/", views.css, name="css"),
-    path("django/", views.django, name="django"),
-    path("git/", views.git, name="git"),
-    path("html/", views.html, name="html"),
-    path("python/", views.python, name="python")
+    path("<str:title>/", views.load_content, name="load_content")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,32 +2,18 @@ from django.shortcuts import render
 
 from . import util
 
-from markdown2 import Markdown
-markdowner = Markdown()
+from markdown2 import markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def css(request):
-    with open("entries/CSS.md") as f:
+def load_content(request, title):
+    with open(f"entries/{title}.md") as f:
         s = f.read()   
-        print(markdowner.convert(s))
         return render(request, "encyclopedia/show_result.html", {
-            "page_title": "CSS",
-            "content": markdowner.convert(s)
+            "page_title": title,
+            "content": markdown(s)
         })
-
-def django(request):
-    return render(request, "encyclopedia/show_result.html")
-
-def git(request):
-    return render(request, "encyclopedia/show_result.html")
-
-def html(request):
-    return render(request, "encyclopedia/show_result.html")
-
-def python(request):
-    return render(request, "encyclopedia/show_result.html")
 
