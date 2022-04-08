d1b41db89ebda28f225bba5224d30528e200b5fa
Chuang Bing Cheng <tars3017@debian-BULLSEYE-live-builder-AMD64>
2022-04-08 22:03:54 +0800

    Finally finish the search totally match function, ready to make fuzzy search function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -6,12 +6,16 @@ from markdown2 import markdown
 
 from django import forms
 
+from django.http import HttpResponseRedirect
+
 class NewSearchForm(forms.Form):
-    word = forms.CharField(label="search for")
+    word = forms.CharField(label="search for", widget=forms.TextInput(attrs={'class': 'search'}))
+    
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": NewSearchForm()
     })
 
 def load_content(request, title):
@@ -20,12 +24,28 @@ def load_content(request, title):
             s = f.read()   
             return render(request, "encyclopedia/show_page.html", {
                 "page_title": title,
-                "content": markdown(s)
+                "content": markdown(s),
+                "form": NewSearchForm()
             })
-    return render(request, "encyclopedia/error.html")
+    return render(request, "encyclopedia/error.html", {"form": NewSearchForm})
 
 def search(request):
+    if request.method == 'POST':
+        form = NewSearchForm(request.POST)
+        if form.is_valid():
+            keyword = form.cleaned_data["word"]
+            print(keyword)
+
+            if (keyword in util.list_entries()):
+                with open(f"entries/{keyword}.md") as f:
+                    s = f.read()
+                    return render(request, "encyclopedia/show_page.html", {
+                        "page_title": keyword,
+                        "content": markdown(s),
+                        "form": NewSearchForm()
+                    })
     return render(request, "encyclopedia/search.html", {
-        "form": NewSearchForm()
+        "form": NewSearchForm(),
+        "keyword": keyword
     })
 
